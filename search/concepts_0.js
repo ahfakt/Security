ad8af80df0b65a0d2d2ee27f132bdec1c7d00ac2
ahfakt <a.farukakts@outlook.com>
2022-01-17 22:58:08 +0300

    .

--- search/concepts_0.js
+++ search/concepts_0.js
@@ -1,8 +1,7 @@
 var searchData=
 [
   ['constructible_0',['Constructible',['../../Stream/db/d8f/conceptStream_1_1Constructible.html',1,'Stream']]],
-  ['constructibledeserializable_1',['ConstructibleDeserializable',['../../Stream/d6/d37/conceptStream_1_1ConstructibleDeserializable.html',1,'Stream']]],
-  ['deserializable_2',['Deserializable',['../../Stream/d3/d34/conceptStream_1_1Deserializable.html',1,'Stream']]],
-  ['extractable_3',['Extractable',['../../Stream/d4/d91/conceptStream_1_1Extractable.html',1,'Stream']]],
-  ['trivialdeserializable_4',['TrivialDeserializable',['../../Stream/df/d1b/conceptStream_1_1TrivialDeserializable.html',1,'Stream']]]
+  ['deserializable_1',['Deserializable',['../../Stream/d3/d34/conceptStream_1_1Deserializable.html',1,'Stream']]],
+  ['extractable_2',['Extractable',['../../Stream/d4/d91/conceptStream_1_1Extractable.html',1,'Stream']]],
+  ['serializable_3',['Serializable',['../../Stream/de/d51/conceptStream_1_1Serializable.html',1,'Stream']]]
 ];

dac1435afbb1712c422424460c596aa015063580
ahfakt <a.farukakts@outlook.com>
2022-08-04 02:16:42 +0300

    .

--- search/concepts_0.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['constructible_0',['Constructible',['../../Stream/db/d8f/conceptStream_1_1Constructible.html',1,'Stream']]],
-  ['deserializable_1',['Deserializable',['../../Stream/d3/d34/conceptStream_1_1Deserializable.html',1,'Stream']]],
-  ['extractable_2',['Extractable',['../../Stream/d4/d91/conceptStream_1_1Extractable.html',1,'Stream']]],
-  ['serializable_3',['Serializable',['../../Stream/de/d51/conceptStream_1_1Serializable.html',1,'Stream']]]
-];

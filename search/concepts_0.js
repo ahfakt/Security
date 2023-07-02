e62653c1430623277896fdb427ebacf284239f8d
ahfakt <a.farukakts@outlook.com>
2023-07-02 20:01:15 +0300

    .

---
+++ search/concepts_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['char_0',['Char',['../../Stream/d1/d93/conceptStream_1_1Char.html',1,'Stream']]],
+  ['integer_1',['Integer',['../../Stream/d4/d6d/conceptStream_1_1Integer.html',1,'Stream']]],
+  ['signedint_2',['SignedInt',['../../Stream/d3/db1/conceptStream_1_1SignedInt.html',1,'Stream']]],
+  ['unsignedint_3',['UnsignedInt',['../../Stream/d9/dce/conceptStream_1_1UnsignedInt.html',1,'Stream']]]
+];
--- search/searchdata.js
+++ search/searchdata.js
@@ -4,7 +4,8 @@ var indexSectionsWithContent =
   1: "abcdefikmopst",
   2: "s",
   3: "abcfgilps~",
-  4: "der"
+  4: "der",
+  5: "s"
 };
 
 var indexSectionNames =
@@ -13,7 +14,8 @@ var indexSectionNames =
   1: "classes",
   2: "namespaces",
   3: "functions",
-  4: "enums"
+  4: "enums",
+  5: "concepts"
 };
 
 var indexSectionLabels =
@@ -22,6 +24,7 @@ var indexSectionLabels =
   1: "Classes",
   2: "Namespaces",
   3: "Functions",
-  4: "Enumerations"
+  4: "Enumerations",
+  5: "Concepts"
 };
 
